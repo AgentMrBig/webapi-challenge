75561aa74eda00c4aea9b653523055536728c1d3
Eric Hendrix <agentmrbig@gmail.com>
2019-08-05 23:24:26 -0400

    I believe I have mvp, Tested on postman

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const app = express()
 
-
+const projectsRouter = require('./routes/projects')
 
 app.use(express.json())
+app.use('/api/projects', projectsRouter)
 
 
 app.get('/', (req, res) => {
